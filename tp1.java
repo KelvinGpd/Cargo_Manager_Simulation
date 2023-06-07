184f9acb1563dfc20cbf6c888347feb37fa6f439
Kelvin Chen <kelvgpd@gmail.com>
2023-06-07 15:03:14 -0400

    convert coordinates to distance and associate to boxValue. Added tests

--- tp1.java
+++ tp1.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,14 +8,36 @@ import java.io.LineNumberReader;
  * tp1
  */
 
+class Tuple {
+    private double doubleValue;
+    private int intValue;
+
+    public Tuple(double doubleValue, int intValue) {
+        this.doubleValue = doubleValue;
+        this.intValue = intValue;
+    }
+
+    public double getDoubleValue() {
+        return doubleValue;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+}
+
 class solution {
     private int[] boxes;
     private double[][] boxesPosition;
     private int boxAmount;
     private int truckMax;
+    private double[] truckCoords;
+    private ArrayList<Tuple> haversine;
+
 
     public void launch() {
         parseFile();
+        findNextStop();
     }
 
     public void parseFile() {
@@ -69,14 +89,95 @@ class solution {
 
     }
 
-    public void merge(int[] arr1, int[] arr2){
-        // On fait un troixieme array de la taille des deux autres
+    public void convertToDist(double[] truckCoords) {
+        int r = 6371000; //earth radius
+        haversine = new ArrayList<>();
+        int i = 0;
+        for (double[] position : boxesPosition) {
+            int amount = boxes[i];
+            i++;
+            double lat = position[0];
+            double lon = position[1];
+            //haversine
+            double in = Math.sqrt(Math.pow(Math.sin((lat-truckCoords[0])/2), 2) +
+                    Math.cos(truckCoords[0]) * Math.cos(lat) * Math.pow(Math.sin((lon-truckCoords[1])/2), 2));
+            double distance = 2 * r * Math.asin(in);
+            haversine.add(new Tuple(distance, amount));
+        }
+    }
 
+    /* Algorithm */
+    public void findNextStop() {
+        //Initialize Truck Position
+        int max[] = findMaxBoxes();
+        updateState(max);
+        //Initialize truck coordinates
+        truckCoords = boxesPosition[max[1]];
+        convertToDist(truckCoords);
+        do {
+            //Traverse
+        }while (boxAmount != truckMax);
+    }
+
+    public int[] findMaxBoxes() {
+        int temp = Integer.MIN_VALUE;
+        int position = -1;
+        for (int i = 0; i < boxes.length; i++) {
+            int box = boxes[i];
+            if (box > temp) {
+                temp = box;
+                position = i;
+            }
+        }
+        int max[] = new int[2];
+        max[0] = temp;
+        max[1] = position;
+        return max;
+    }
+
+    public void updateState(int[] info) {
+        int addedBoxes = info[0];
+        int newAmount = boxAmount + addedBoxes;
+        //update cargo amount
+        if( newAmount > truckMax ) {
+            boxAmount = truckMax;
+            //update Warehouse content TODO
+        }else {
+            boxAmount = newAmount;
+            //update Warehouse content TODO
+        }
+    }
+
+    public void merge(int[] arr1, double[][] arr2){
+        // On fait un troisieme array de la taille des deux autres
+        // [ (int,[double, double]) ]
 
     }
+
     public void sort(){
 
     }
+
+
+    /* Testing purposes */
+
+    public double[][] getBoxesPosition(){
+        return boxesPosition;
+    }
+    public int[] getBoxes(){
+        return boxes;
+    }
+    public int getBoxAmount(){
+        return boxAmount;
+    }
+    public int getTruckMax(){
+        return truckMax;
+    }
+
+    public ArrayList<Tuple> getHaversine(){
+        return haversine;
+    }
+
 
 }
 
@@ -85,5 +186,31 @@ public class tp1 {
     public static void main(String[] args) {
         solution sol = new solution();
         sol.launch();
+
+        /* Testing purposes */
+
+        ArrayList<Tuple> haver = sol.getHaversine();
+        for (Tuple some : haver){
+            System.out.println(some.getDoubleValue()+ " " + some.getIntValue());
+        }
+        System.out.println();
+
+        int[] boxes = sol.getBoxes();
+        System.out.print("Boxes: ");
+        for (int box : boxes) {
+            System.out.print(box + " ");
+        }
+        System.out.println();
+
+
+        double[][] boxesPosition = sol.getBoxesPosition();
+        System.out.println("Boxes Position:");
+        for (double[] position : boxesPosition) {
+            for (double coordinate : position) {
+                System.out.print(coordinate + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
